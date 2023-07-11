350ef84b2ffff69e38229d9865d375fdec8ac47e
CJFeagin33 <135062039+CJFeagin33@users.noreply.github.com>
2023-07-10 18:57:57 -0700

    added begin quiz btn functionality

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 var highScoresButton = document.querySelector("#highScoresButton");
 var returnToHomePageButton = document.querySelector("#backToHome-icon")
-
+var beginQuizButton = document.querySelector("#beginQuizButton")
 
 
 
 var visitHighScores = function() {
   var landingPage = document.getElementById("landing-page");
-  console.log(landingPage)
   landingPage.style.display = "none";
   var highScoresPage = document.getElementById("HighScores-page");
-  console.log(highScoresButton)
   highScoresPage.style.display = "inline";
   var returnHomeBtn = document.getElementById("backToHome-icon");
   returnHomeBtn.style.display = "inline";
@@ -19,5 +17,15 @@ var returnToHomePage = function() {
   window.location.reload();
 }
 
+var question1Page = function() {
+  var landingPage = document.getElementById("landing-page");
+  landingPage.style.display = "none";
+  var question1 = document.getElementById("question1-page")
+  question1.style.display = "inline"
+  var returnHomeBtn = document.getElementById("backToHome-icon");
+  returnHomeBtn.style.display = "inline";
+}
+
 highScoresButton.addEventListener("click", visitHighScores);
 returnToHomePageButton.addEventListener("click", returnToHomePage);
+beginQuizButton.addEventListener("click", question1Page)
